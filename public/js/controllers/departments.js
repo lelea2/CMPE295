7fb0a64d5e0ec9d36d854365187625db3f24ee71
lelea2 <daomaikhanh1988@yahoo.com>
2017-01-10 00:11:10 -0800

    Initial check in '

--- public/js/controllers/departments.js
+++ public/js/controllers/departments.js
@@ -1,6 +1,7 @@
 App.controller('departmentsController', ['$scope', '$http', function ($scope, $http) {
 
   $scope.departments = [];
+  $scope.formDepartment = {};
 
   $scope.init = function() {
     $(document).trigger('linkedgov:loading_start');
@@ -12,6 +13,36 @@ App.controller('departmentsController', ['$scope', '$http', function ($scope, $h
       //success, load to view process
       $(document).trigger('linkedgov:loading_stop');
       $scope.departments = resp.data;
+    });
+  };
+
+  $scope.editDepartment = function(id) {
+    $scope.formDepartment = $scope.getCurrentDepartment(id) || {};
+    // console.log($scope.formOffice);
+    $('#myModal').modal({
+      show: true
+    });
+  };
+
+  $scope.getCurrentDepartment = function(id) {
+    if (!!id && $scope.departments.length > 0) {
+      for (var i = 0; i < $scope.departments.length; i++) {
+        if ($scope.departments[i].id === id) {
+          return $scope.departments[i];
+        }
+      }
+    }
+  };
+
+  $scope.updateDepartment = function(department_id) {
+    $http({
+      method: 'GET',
+      headers: LINKEDGOV.getHeaders(true),
+      url: '/api/departments/' + $scope.formDepartment.id,
+      data: $scope.formDepartment
+    }).then(function(resp) {
+      //success, load to view process
+      window.location.reoload();
     });
   };
 
