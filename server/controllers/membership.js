2c258413404d3753fc9461c8e000aa50181567d2
lelea2 <daomaikhanh1988@yahoo.com>
2017-01-22 13:46:25 -0800

    Initial check in

--- server/controllers/membership.js
+++ server/controllers/membership.js
@@ -2,9 +2,29 @@
 
 var Membership = require('../models/').Memberships,
     User = require('../models/').Users,
-    Role = require('../models/').Roles;
+    Role = require('../models/').Roles,
+    uuid = require('node-uuid');
 
 module.exports = {
+
+  create(req, res) {
+    var id =  uuid.v4();
+    var data = req.body;
+    Membership.create({
+      id: id,
+      user_id: data.user_id,
+      group_id: data.group_id,
+      group_type: data.group_type,
+      role_id: data.role_id,
+      permission_id: data.permission_id
+    })
+    .then(function (newRecord) {
+      res.status(201).json(newRecord);
+    })
+    .catch(function (error) {
+      res.status(500).json(error);
+    });
+  },
 
   show(req, res) {
     Membership.findAll({
