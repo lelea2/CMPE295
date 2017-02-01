c5ec0450309ff752124d6c6299017fff2190d022
kdao <khanh.dao@move.com>
2017-02-01 10:26:36 -0800

    Adding tasks

--- public/js/controllers/task_case.js
+++ public/js/controllers/task_case.js
@@ -1,9 +1,16 @@
 App.controller('taskCaseController', ['$scope', '$http', function ($scope, $http) {
 
-  $scope.case = [];
+  $scope.tasks = [];
 
   $scope.init = function() {
-
+    $http({
+      method: 'GET',
+      headers: LINKEDGOV.getHeaders(true),
+      url: '/api/tasks'
+    }).then(function(resp) {
+      //success, load to view process
+      $scope.tasks = resp.data;
+    });
   };
 
 }]);
