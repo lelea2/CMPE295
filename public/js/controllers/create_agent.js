c921accd31d5f3120fe0ad9cb076be19a6857219
kdao <khanh.dao@move.com>
2017-02-02 21:28:34 -0800

    Initial check in

--- public/js/controllers/create_agent.js
+++ public/js/controllers/create_agent.js
@@ -1,12 +1,58 @@
 App.controller('createAgentController', ['$scope', '$http', function ($scope, $http) {
 
   $scope.formAgent = {};
+  $scope.roles = [];
+  $scope.departments = [];
+  $scope.offices = [];
+  $scope.currentDepartmentId = null;
+  $scope.showAgentForm = true;
 
   $scope.init = function() {
 
   };
 
+  $scope.getRoles = function() {
+    $http({
+      method: 'GET',
+      headers: LINKEDGOV.getHeaders(true),
+      url: '/api/roles'
+    }).then(function(resp) {
+      $scope.roles = resp.data;
+    });
+  };
+
+  $scope.getDepartments = function() {
+    $http({
+      method: 'GET',
+      headers: LINKEDGOV.getHeaders(true),
+      url: '/api/departments'
+    }).then(function(resp) {
+      $scope.departments = resp.data;
+    });
+  };
+
+  $scope.getOffices = function() {
+    $http({
+      method: 'GET',
+      headers: LINKEDGOV.getHeaders(true),
+      url: '/api/offices?department_id=' + $scope.currentDepartmentId
+    }).then(function(resp) {
+      $scope.offices = resp.data;
+    });
+  };
+
   $scope.createAgent = function() {
+    $http({
+      method: 'POST',
+      headers: LINKEDGOV.getHeaders(true),
+      url: '/api/agents',
+      data: $scope.formAgent
+    }).then(function(resp) {
+
+    });
+  };
+
+  $scope.createMembership = function() {
 
   };
 
