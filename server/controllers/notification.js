420cb2d69ba85565da1814f923ce2deb2132c90e
kdao <khanh.dao@move.com>
2017-02-03 10:57:07 -0800

    Initial check in for notifications

--- server/controllers/notification.js
+++ server/controllers/notification.js
@@ -6,7 +6,18 @@ var Notification = require('../models/').Notifications,
 module.exports = {
 
   show(req, res) {
-
+    var dataBody = {};
+    Notification.findAll({
+      where: {
+        task_type: 'workflow'
+      }
+    })
+    .then(function (data) {
+      res.status(200).json(data);
+    })
+    .catch(function (error) {
+      res.status(500).json(error);
+    });
   },
 
   create(req, res) {
