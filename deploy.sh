6d2a9bcaf197acee42ac15be47ece3ba1fd47fe4
kdao <khanh.dao@move.com>
2017-02-03 12:12:55 -0800

    Trying deployment

--- deploy.sh
+++ deploy.sh
@@ -5,17 +5,17 @@ JQ="jq --raw-output --exit-status"
 
 configure_aws_cli(){
   aws --version
-  aws configure set default.region us-east-1
+  aws configure set default.region us-west-2
   aws configure set default.output json
 }
 
 deploy_cluster() {
 
-  family="sample-webapp-task-family"
+  family="linkgov-app-task-family"
 
   make_task_def
   register_definition
-  if [[ $(aws ecs update-service --cluster sample-webapp-cluster --service sample-webapp-service --task-definition $revision | \
+  if [[ $(aws ecs update-service --cluster linkgov-app-cluster --service linkgov-app-service --task-definition $revision | \
                  $JQ '.service.taskDefinition') != $revision ]]; then
     echo "Error updating service."
     return 1
@@ -24,7 +24,7 @@ deploy_cluster() {
   # wait for older revisions to disappear
   # not really necessary, but nice for demos
   for attempt in {1..30}; do
-    if stale=$(aws ecs describe-services --cluster sample-webapp-cluster --services sample-webapp-service | \
+    if stale=$(aws ecs describe-services --cluster linkgov-app-cluster --services linkgov-app-service | \
                    $JQ ".services[0].deployments | .[] | select(.taskDefinition != \"$revision\") | .taskDefinition"); then
       echo "Waiting for stale deployments:"
       echo "$stale"
@@ -41,8 +41,8 @@ deploy_cluster() {
 make_task_def(){
   task_template='[
     {
-      "name": "go-sample-webapp",
-      "image": "%s.dkr.ecr.us-west-2.amazonaws.com/go-sample-webapp:%s",
+      "name": "linkgov-app",
+      "image": "%s.dkr.ecr.us-west-2.amazonaws.com/linkgov-app:%s",
       "essential": true,
       "memory": 200,
       "cpu": 10,
@@ -60,7 +60,7 @@ make_task_def(){
 
 push_ecr_image(){
   eval $(aws ecr get-login --region us-west-2)
-  docker push $AWS_ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/go-sample-webapp:$CIRCLE_SHA1
+  docker push $AWS_ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/linkgov-app:$CIRCLE_SHA1
 }
 
 register_definition() {
