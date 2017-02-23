9383bbef404316e9f7d57560aebe146071ec4c5f
kdao <khanh.dao@move.com>
2017-02-23 14:17:14 -0800

    Initial check in

--- test/index.js
+++ test/index.js
@@ -1,6 +1,11 @@
 'use strict';
 
 var subject = require('../index.js');
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var should = chai.should();
+
+chai.use(chaiHttp);
 
 describe('index', function() {
   it('Should run', function(done) {
