2fd345f22af9c57f80d09d3e6386a406f0054ffb
kdao <khanh.dao@move.com>
2017-04-16 12:40:02 -0700

    Merge heads

--- public/js/controllers/agents_office.js
+++ public/js/controllers/agents_office.js
@@ -1,7 +1,43 @@
 App.controller('agentsController', ['$scope', '$http', function ($scope, $http) {
 
+  $scope.membership = {};
+  $scope.agents = [];
+
   $scope.init = function() {
+    $(document).trigger('linkedgov:loading_start');
+    $http({
+      method: 'GET',
+      headers: LINKEDGOV.getHeaders(true),
+      url: '/api/agent_membership'
+    }).then(function(resp) {
+      //success, load to view process
+      console.log(resp.data);
+      $scope.membership = resp.data;
+      if ($scope.membership.group_type === 'office') {
+        $scope.getOffice();
+      } else {
+        alert('Current member has not assigned membership for office');
+        $(document).trigger('linkedgov:loading_stop');
+      }
+    });
+  };
+
+  $scope.getOffice = function() {
+    $http({
+      method: 'GET',
+      headers: LINKEDGOV.getHeaders(true),
+      url: '/api/offices/' + $scope.membership.group_id
+    }).then(function(resp) {
+      console.log(resp.data);
+      $scope.membership.Office = resp.data;
+      console.log($scope.membership);
+      $(document).trigger('linkedgov:loading_stop');
+    });
+  };
+
+  $scope.getAgents = function() {
 
   };
+
 
 }]);
