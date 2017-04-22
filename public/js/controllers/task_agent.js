fb018af0ae792af5025eff63545e1f1c0a9629b8
kdao <khanh.dao@move.com>
2017-04-22 14:40:44 -0700

    Initial check in

--- public/js/controllers/task_agent.js
+++ public/js/controllers/task_agent.js
@@ -2,6 +2,8 @@ App.controller('tasksController', ['$scope', '$http', function ($scope, $http) {
 
   $scope.membership = {};
   $scope.tasks = [];
+  $scope.agents = [];
+  $scope.task_assignee = {};
 
   $scope.init = function() {
     $(document).trigger('linkedgov:loading_start');
@@ -48,8 +50,24 @@ App.controller('tasksController', ['$scope', '$http', function ($scope, $http) {
     });
   };
 
-  $scope.viewDetail = function(item) {
+  $scope.getAgents = function() {
+    $(document).trigger('linkedgov:loading_start');
+    $scope.agents = [];
+    $http({
+      method: 'GET',
+      headers: LINKEDGOV.getHeaders(true),
+      url: '/api/memberships?group_type=office&group_id=' + $scope.membership.group_id
+    }).then(function(resp) {
+      //success, load to view process
+      $(document).trigger('linkedgov:loading_stop');
+      $scope.agents = resp.data;
+    });
+  };
 
+  $scope.viewDetail = function(item) {
+    console.log('view task detail...');
+    console.log(item);
+    $scope.getAgents();
   };
 
   $scope.formatDate = function(date) {
